7ff699300a3b105d1a78a1ff018a14df0a3853fc
pablo <pablo@gmail.com>
2022-10-27 08:37:57 -0500

    MI PROYECTO

---
+++ episodio/app.js
@@ -0,0 +1,25 @@
+const apiMorty = async(pagina) => {
+    let url = "https://rickandmortyapi.com/api/episode/?page=" + pagina;
+    const api = await fetch(url);
+    const data = await api.json();
+    console.log(data);
+    divRes = document.querySelector("#resultado1");
+    divRes.innerHTML = ""
+    data.results.map(item => {
+        divItem = document.createElement('div');
+        divItem.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Episode:</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${item.air_date}</h6>
+          <p class="card-text">${item.name}</p>
+          <p class="card-text">${item.episode}</p>
+          <p class="card-text">${item.id}</p>
+          <p class="card-text">${item.created}</p>
+        </div>
+      </div>
+      `
+        divRes.appendChild(divItem);
+    });
+}
+apiMorty(1);
---
+++ location/app.js
@@ -0,0 +1,24 @@
+const apiMorty = async(pagina) => {
+    let url = "https://rickandmortyapi.com/api/location/?page=" + pagina;
+    const api = await fetch(url);
+    const data = await api.json();
+    console.log(data);
+    divRes = document.querySelector("#resultado1");
+    divRes.innerHTML = ""
+    data.results.map(item => {
+        divItem = document.createElement('div');
+        divItem.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">LOCATION:</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${item.id}</h6>
+          <p class="card-text">${item.created}</p>
+          <p class="card-text">${item.dimension}</p>
+          <p class="card-text">${item.name}</p>
+        </div>
+      </div>
+      `
+        divRes.appendChild(divItem);
+    });
+}
+apiMorty(1);
